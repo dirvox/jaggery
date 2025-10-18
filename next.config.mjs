ec69a338364ce3876f7cf973af8357f66050046f
Devansh Jinraniya <devanshjinraniya41@gmail.com>
2025-10-19 00:06:56 +0530

    third commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export', // enable static export
+  experimental: {
+    turbo: true, // optional, because you are using turbopack
+  },
+};
+
+
 
 export default nextConfig;
