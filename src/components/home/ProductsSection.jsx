e72446782ccdad76224e37652bacf2db98e30010
Devansh Jinraniya <devanshjinraniya41@gmail.com>
2025-10-19 11:39:56 +0530

    jdhhjbd

--- src/components/home/ProductsSection.jsx
+++ src/components/home/ProductsSection.jsx
@@ -14,6 +14,8 @@ const ProductsSection = () => {
 
 
 useEffect(() => {
+
+  console.log("use effect called function")
     const checkBackend = async () => {
       try {
         const response = await axios.get(`${baseUrl}api/items/democheck`);
